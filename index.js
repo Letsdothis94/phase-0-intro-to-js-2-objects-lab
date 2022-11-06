ef8e8f237c8f6ba85c963f68a05e117de57c970a
Cristian Contreras <contreras.cristian994@gmail.com>
2022-11-06 14:24:31 -0500

    commit tree aa3c84bacb17043782daf63eb04d21b3c15fb1a9

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "cristian",
+    streetAddress: "11 street",
+}
+
+function updateEmployeeWithKeyAndValue() {
+    const newKey = { ...employee };
+    newKey.streetAddress = "11 Broadway";
+    return newKey;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue() {
+    employee.streetAddress = "12 Broadway";
+    return employee;
+}
+function deleteFromEmployeeByKey() {
+    const deleteKey = { ...employee };
+    delete deleteKey.streetAddress;
+    delete deleteKey.name;
+    return deleteKey;
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee.name;
+    delete employee.streetAddress;
+    return employee;
+}
